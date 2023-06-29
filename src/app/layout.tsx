69627afb51d59a9ab49508239a340c3c1f73b80f
joaorodriguesjr <mail@joaorodriguesjr.com>
2023-06-29 18:38:45 -0300

    change icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <link rel="icon" href="favicon.svg" type="image/svg+xml" />
       <body className={inter.className}>{children}</body>
     </html>
   )
